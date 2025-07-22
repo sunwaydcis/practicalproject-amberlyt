b2d08b1111a9d5f9efe3a4057a44c17da66c709d
ambs <22094775@imail.sunway.edu.my>
2025-07-22 18:06:11 +0800

    update

--- src/main/scala-3/MainApp.scala
+++ src/main/scala-3/MainApp.scala
@@ -1,51 +1,39 @@
 package ch.makery.address
 
 import ch.makery.address.model.Person
+import ch.makery.address.view.{PersonEditDialogController, PersonOverviewController}
 import javafx.fxml.FXMLLoader
+import javafx.scene as jfxs
+import scalafx.Includes.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.Scene
-import scalafx.Includes.*
-import javafx.scene as jfxs
-import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
-import ch.makery.address.view.PersonEditDialogController
-import scalafx.stage.Stage
-import scalafx.stage.Modality
-
-
+import scalafx.scene.Scene
+import scalafx.stage.{Modality, Stage}
 
 object MainApp extends JFXApp3:
 
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  var personOverviewControl: Option[PersonOverviewController] = None
+  // ✅ declare controller
 
-  /**
-   * The data as an observable list of Persons.
-   */
   val personData = new ObservableBuffer[Person]()
-
-  /**
-   * Constructor
-   */
-  personData += new Person("Hans", "Muster")
-  personData += new Person("Ruth", "Mueller")
-  personData += new Person("Heinz", "Kurz")
-  personData += new Person("Cornelia", "Meier")
-  personData += new Person("Werner", "Meyer")
-  personData += new Person("Lydia", "Kunz")
-  personData += new Person("Anna", "Best")
-  personData += new Person("Stefan", "Meier")
-  personData += new Person("Martin", "Mueller")
-
-
-  // ... THE REST OF THE CLASS ...
-
+  personData ++= Seq(
+    Person("Hans", "Muster"),
+    Person("Ruth", "Mueller"),
+    Person("Heinz", "Kurz"),
+    Person("Cornelia", "Meier"),
+    Person("Werner", "Meyer"),
+    Person("Lydia", "Kunz"),
+    Person("Anna", "Best"),
+    Person("Stefan", "Meier"),
+    Person("Martin", "Mueller")
+  )
 
   override def start(): Unit =
     val rootResource = getClass.getResource("view/RootLayout.fxml")
     val loader = new FXMLLoader(rootResource)
     loader.load()
-
     roots = Option(loader.getRoot[jfxs.layout.BorderPane])
 
     stage = new PrimaryStage():
@@ -53,18 +41,21 @@ object MainApp extends JFXApp3:
       scene = new Scene():
         root = roots.get
 
-      showPersonOverview()
-    def showPersonOverview(): Unit =
-      val resource = getClass.getResource("view/PersonOverview.fxml")
-      val loader = FXMLLoader(resource)
-      loader.load()
-      val roots = loader.getRoot[jfxs.layout.AnchorPane]
-      this.roots.get.center = roots
+    showPersonOverview()
+
+  def showPersonOverview(): Unit =
+    val resource = getClass.getResource("view/PersonOverview.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load()
+    val overviewRoot = loader.getRoot[javafx.scene.layout.AnchorPane]
+    personOverviewControl = Some(loader.getController[PersonOverviewController])
+    roots.get.center = overviewRoot
+
 
   def showPersonEditDialog(person: Person): Boolean =
     val resource = getClass.getResource("view/PersonEditDialog.fxml")
     val loader = new FXMLLoader(resource)
-    loader.load();
+    loader.load()
     val roots2 = loader.getRoot[jfxs.Parent]
     val control = loader.getController[PersonEditDialogController]
 
